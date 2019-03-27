2035b06f7667ff9631d7b549fe87c0a727551185
qianqianya <1683472461@qq.com>
2019-03-27 08:41:34 +0000

    web.php

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//登陆
+Route::any('/api/login', 'User\UserController@login');
+
+//个人中心
+Route::any('/api/token', 'User\UserController@token');
+
+
+//注册
+Route::any('/reg','Register\RegisterController@doReg');
+
+
+#####################################
+//个人中心
+Route::any('/api/goodsList', 'Goods\GoodsController@goodsList');
+
+
+
+Route::any('/api/goodsDetails', 'Goods\GoodsController@goodsDetails');
