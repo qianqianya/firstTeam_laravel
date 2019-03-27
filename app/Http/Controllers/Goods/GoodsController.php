c96452ea6cf4bc85a2f2a19e05b2ab1aaacc4684
qianqianya <1683472461@qq.com>
2019-03-27 08:47:10 +0000

    request

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 
 class GoodsController extends Controller
