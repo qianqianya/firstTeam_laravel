25c0331b9c04334048584f45888ece77f35cfae0
qianqianya <1683472461@qq.com>
2019-03-28 11:47:08 +0000

    ,

--- app/Http/Controllers/Like/LikeController.php
+++ app/Http/Controllers/Like/LikeController.php
@@ -73,16 +73,15 @@ class LikeController extends Controller
         if($res){
 
            $obj = DB::table('laravel_like')->join('laravel_like_num','laravel_like.goods_id','=','laravel_like_num.goods_id')->where(['u_id'=>$u_id])->first();
-           $arr = json_decode($obj,true) ;
-           $arr['like_status']= 1;
-           return json_encode($arr);
+           $obj->like_status = 1;
+           return json_encode($obj);
         }else{
 
            $num = DB::table('laravel_like_num')->where(['goods_id'=>$goods_id])->first();
-            $arr = json_decode($num,true) ;
-            $arr['like_status']= 2;
+           $num->like_status = 2;
+
            if($num){
-               return json_encode($arr);
+               return json_encode($num);
            }else{
 
                return json_encode(['like_name'=>0,'like_status'=>3]);
