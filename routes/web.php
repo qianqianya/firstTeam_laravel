2aa0881f9a0dd9e16ee2f196cb2f707bf23f7b0e
qianqianya <1683472461@qq.com>
2019-03-28 11:06:27 +0000

    ..

--- routes/web.php
+++ routes/web.php
@@ -53,10 +53,9 @@ Route::any('/quit','User\UserController@quit');
 Route::any('/api/like','Like\LikeController@like');
 
 
-<<<<<<< HEAD
+
 //展示点赞
 Route::any('/api/likecheck','Like\LikeController@likecheck');
-=======
 
 
 
@@ -68,4 +67,4 @@ Route::any('/api/likecheck','Like\LikeController@likecheck');
 
 //收藏
 Route::any('/collect','Register\CollectController@collect');
->>>>>>> master
+
