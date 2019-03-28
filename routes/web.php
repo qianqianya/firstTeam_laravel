d5770668a5c9e9e456689d76e1840d9ac5913b74
qianqianya <1683472461@qq.com>
2019-03-28 10:21:06 +0800

    first

--- routes/web.php
+++ routes/web.php
@@ -43,36 +43,6 @@ Route::any('/api/goodsDetails', 'Goods\GoodsController@goodsDetails');
 
 //退出
 Route::any('/quit','User\UserController@quit');
-=======
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//登陆
-Route::any('/api/login', 'User\UserController@login');
-
-//个人中心
-Route::any('/api/token', 'User\UserController@token');
-
-
-//注册
-Route::any('/reg','Register\RegisterController@doReg');
-
-//个人中心
-Route::any('/api/goodsList', 'Goods\GoodsController@goodsList');
 
 
 # 添加订单
