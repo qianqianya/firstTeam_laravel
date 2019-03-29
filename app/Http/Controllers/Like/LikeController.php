fdfdd858b21a9cfcd7b890fe63b3328924fc89f0
qianqianya <1683472461@qq.com>
2019-03-29 01:47:17 +0000

    m

--- app/Http/Controllers/Like/LikeController.php
+++ app/Http/Controllers/Like/LikeController.php
@@ -86,9 +86,10 @@ class LikeController extends Controller
         }else{
 
            $num = DB::table('laravel_like_num')->where(['goods_id'=>$goods_id])->first();
-           $num->like_status = 2;
+
 
            if($num){
+               $num->like_status = 2;
                return json_encode($num);
            }else{
 
